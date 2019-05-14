c360d4af08ebbf59baf7c58ac726caf6298c1cd4
JihunPark98 <jihun.park@my.jcu.edu.au>
2019-05-15 00:52:43 +0800

    commit

---
+++ prac_06/Horse.java
@@ -0,0 +1,32 @@
+public class Horse
+{
+    String name;
+    String color;
+    int birthYear;
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public int getBirthYear()
+    {
+        return birthYear;
+    }
+
+    public void setName(String n)
+    {
+        name = n;
+    }
+    public void setColor(String c)
+    {
+        color = c;
+    }
+    public void setBirthYear(int y)
+    {
+        birthYear = y;
+    }
+}
